1f452249c2fef69f4dabd1d9832ef93d15b05382
Heba1998 <hebaalmomani1998@gmail.com>
2021-08-01 17:40:47 +0300

    lab 6 done

--- city/README.md
+++ city/README.md
@@ -19,7 +19,63 @@ User want to see a map of the city so that you can see the layout of the area yo
 ## Architecture
 use react and bootstrap
 
+## Time Estimates
+
+```
+Name of feature: Set up my React repository & API keys.
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 1:00 pm
+
+Finish time: 2:00 pm
+
+Actual time needed to complete:  1 hour
+
+```
+
+```
+Name of feature:  The ability to search for a new site
+
+Estimate of time needed to complete: 40 minute
+
+Start time: 2:00 pm
+
+Finish time: 2:40 pm
+
+Actual time needed to complete: 40 minute
+
+```
+
+```
+Name of feature:  Show Map
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 2:40 pm
+
+Finish time: 3:40 pm
+
+Actual time needed to complete: 1 hour
+
+```
+
+```
+Name of feature: show Errors
+
+Estimate of time needed to complete: 1 and half hour
+
+Start time: 3:40 pm
+
+Finish time: 5:20 pm
+
+Actual time needed to complete: 1 and half hour
+
+```
+
 ## Change Log
+0q-08-2001 5:31pm - Application now is done.
 
 
-## Credit and Collaborations
+
+
--- city/src/App.js
+++ city/src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import Card from 'react-bootstrap/Card';
+import Alert from 'react-bootstrap/Alert';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      displayName:'',
+      longitude:'',
+      latitude:'',
+      ErrorMSG: '',
+      error:false,
+      show:false,
+    }
+  }
+  
+  submitForm =(e)=>{
+    this.setState({
+      displayName: e.target.value,
+      
+    })
+  }
+
+  submitData=async (e)=>{
+    e.preventDefault()
+    try{ let  response= await axios.get(`https://eu1.locationiq.com/v1/search.php?key=pk.e266821f0ad39e21a46098fffe81ff92&city=${this.state.displayName}&format=json
+    
+    `)
+   
+    this.setState({
+      displayName:  response.data[0].display_name,
+      longitude:  response.data[0].lon,
+      latitude:  response.data[0].lat, 
+      show: true
+      
+      
+    })
+    }
+    catch(ErrorMSG){
+      this.setState({
+        ErrorMSG: 'ERROR :Please Enter City Name',
+        error: true
+      })
+    }
+  }
+  render() {
+    return (
+      <div>
+
+        
+
+        <form onSubmit={(e)=>{this.submitData(e)}}  style={{padding: "10px", borderStyle: "outset" , width : "20rem" , margin: "20px auto" }}>
+          <label>City Explorer</label>
+          <br/>
+          <br/>
+          <input type="text" placeholder="Enetr City Name " onChange={(e)=>{this.submitForm (e)}} />
+          <button>Explore!</button>
+        </form>
+
+       {(this.state.show) &&
+        <Card style={{ width: '25rem' , margin:"30px" , padding: "10px", backgroundColor:" #e0e0d1" }}>
+
+  <Card.Img variant="top" src={`https://maps.locationiq.com/v3/staticmap?key=pk.e266821f0ad39e21a46098fffe81ff92
+        &center=${this.state.latitude},${this.state.longitude}&zoom=15&format=png`} style={{ borderRadius: "8px" }} />
+  <Card.Body>
+    <Card.Title><h3>City: </h3>  {this.state.displayName}</Card.Title>
+    <Card.Text>
+    <h3>longitude:</h3> {this.state.longitude}
+    </Card.Text>
+    <Card.Text>
+    <h3>latitude:</h3>  {this.state.latitude}
+    </Card.Text>
+  </Card.Body>
+</Card>
+     
+        }
+
+        {(this.state.error) &&
+        <Alert variant={'danger'}>{this.state.ErrorMSG} </Alert>
+        
+        }
+        
+      </div>
+    )
+  }
 }
 
 export default App;
